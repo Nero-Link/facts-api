e2b8139328e20187f8948629a07d7f5ab7a0b329
Nero-Link <nerolink1107@gmail.com>
2022-07-21 11:04:51 +1000

    styling done - working on data fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useEffect, useState } from "react";
 import $ from "jquery";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay } from "swiper/core";
@@ -10,49 +10,59 @@ SwiperCore.use([Autoplay]);
 
 function App() {
   const apiKey = "3iLLzYQSIqDPLAugE4VNkQ==yjVVpZ2Z6sN5omTp";
-  const [facts, setFacts] = useState(0);
+  const [facts, setFacts] = useState([]);
+  const [fetched, setFetched] = useState(false);
+
+  useEffect(() => {
+    apiCall();
+  }, []);
 
   function apiCall() {
     const limit = 30;
-    $.ajax({
-      method: "GET",
-      url: "https://api.api-ninjas.com/v1/facts?limit=" + limit,
-      headers: { "X-Api-Key": apiKey },
-      contentType: "application/json",
-      success: function (result) {
-        setFacts(result);
-        console.log(facts);
-      },
-      error: function ajaxError(jqXHR) {
-        console.error("Error: ", jqXHR.responseText);
-      },
-    });
+    if (!fetched) {
+      $.ajax({
+        method: "GET",
+        url: "https://api.api-ninjas.com/v1/facts?limit=" + limit,
+        headers: { "X-Api-Key": apiKey },
+        contentType: "application/json",
+        success: function (result) {
+          setFacts(result);
+          setFetched(true);
+          console.log(facts.forEach((fact) => console.log(fact.fact)));
+        },
+        error: function ajaxError(jqXHR) {
+          console.error("Error: ", jqXHR.responseText);
+        },
+      });
+    }
   }
 
   return (
     <div className="App">
+      <div className="banner">
+        <h2 className="title">Random Fact of the Hour</h2>
+      </div>
       <div className="facts-container">
         <Swiper
           slidesPerView={1}
           loop={true}
           speed={750}
           autoplay={{
-            delay: 5000,
+            delay: 10000,
             disableOnInteraction: false,
           }}
         >
-          <SwiperSlide>
-            <p>1</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p>2</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p>3</p>
-          </SwiperSlide>
+          {apiCall()}
+          {facts.length > 0 &&
+            facts.map((fact) => {
+              return (
+                <SwiperSlide>
+                  <p>{fact.fact}</p>
+                </SwiperSlide>
+              );
+            })}
         </Swiper>
       </div>
-      {/* <button onClick={apiCall()}>New fact</button> */}
     </div>
   );
 }
