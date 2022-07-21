359da21aaa371d23881cd47e9e7c63de52257b54
Nero-Link <nerolink1107@gmail.com>
2022-07-21 13:53:47 +1000

    feature complete

--- src/App.js
+++ src/App.js
@@ -10,37 +10,48 @@ SwiperCore.use([Autoplay]);
 
 function App() {
   const apiKey = "3iLLzYQSIqDPLAugE4VNkQ==yjVVpZ2Z6sN5omTp";
+  const tick = 3600000;
   const [facts, setFacts] = useState([]);
-  const [fetched, setFetched] = useState(false);
+  let [refresh, setRefresh] = useState(0);
+  let [timer, setTimer] = useState(0);
 
   useEffect(() => {
     apiCall();
-  }, []);
+  }, [refresh]);
 
   function apiCall() {
-    const limit = 30;
-    if (!fetched) {
-      $.ajax({
-        method: "GET",
-        url: "https://api.api-ninjas.com/v1/facts?limit=" + limit,
-        headers: { "X-Api-Key": apiKey },
-        contentType: "application/json",
-        success: function (result) {
-          setFacts(result);
-          setFetched(true);
-          console.log(facts.forEach((fact) => console.log(fact.fact)));
-        },
-        error: function ajaxError(jqXHR) {
-          console.error("Error: ", jqXHR.responseText);
-        },
-      });
-    }
+    const limit = 6;
+    $.ajax({
+      method: "GET",
+      url: "https://api.api-ninjas.com/v1/facts?limit=" + limit,
+      headers: { "X-Api-Key": apiKey },
+      contentType: "application/json",
+      success: function (result) {
+        setFacts(result);
+        console.log("API Call Successful");
+      },
+      error: function ajaxError(jqXHR) {
+        console.error("Error: ", jqXHR.responseText);
+      },
+    });
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, tick);
+    return () => clearInterval(interval);
+  }, []);
+
+  if (timer > 0) {
+    setRefresh(refresh + 1);
+    setTimer(0);
   }
 
   return (
     <div className="App">
       <div className="banner">
-        <h2 className="title">Random Fact of the Hour</h2>
+        <h2 className="title">Random Facts of the Hour</h2>
       </div>
       <div className="facts-container">
         <Swiper
@@ -48,11 +59,10 @@ function App() {
           loop={true}
           speed={750}
           autoplay={{
-            delay: 10000,
+            delay: 600000,
             disableOnInteraction: false,
           }}
         >
-          {apiCall()}
           {facts.length > 0 &&
             facts.map((fact) => {
               return (
